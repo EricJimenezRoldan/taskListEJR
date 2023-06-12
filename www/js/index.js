9107f70e10d8cc160d9c9faec23aaca156817ef1
EricJimenezRoldan <astroperix@gmail.com>
2023-06-13 00:41:33 +0200

    base projecte modificada

--- www/js/index.js
+++ www/js/index.js
@@ -21,9 +21,20 @@
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
+$('#buttonAppend').click(addTask);
+
+
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
+}
+
+
+function addTask(){
+    let userTask;
+    userTask = prompt("New task");
+    $("ul").append("<li>" + userTask + "</li>");
+    $("ul").listview("refresh");
 }
